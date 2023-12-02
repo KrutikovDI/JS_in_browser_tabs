5961789cecf4673387c37c5fea3296a52b706812
Danil Krutikov <kdanil.i@yandex.ru>
2023-12-02 13:31:07 +0700

    Initial commit

---
+++ task.js
@@ -0,0 +1,17 @@
+const tabNavigation = document.querySelector(".tab__navigation");
+const tabs = document.querySelectorAll(".tab");
+const tabArray = [...tabs];
+const tabContents = document.querySelectorAll(".tab__content");
+const tabContentsArray = [...tabContents];
+tabNavigation.addEventListener("click", (e) => {
+  let tab = e.target;
+  let index = tabArray.indexOf(tab);
+  for (i = 0; i < tabArray.length; i++) {
+    tabArray[i].className = "tab";
+  }
+  tab.classList.add("tab_active");
+  for (j = 0; j < tabContentsArray.length; j++) {
+    tabContentsArray[j].className = "tab__content";
+  }
+  tabContentsArray[index].classList.add('tab__content_active');
+});
